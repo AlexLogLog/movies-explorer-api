2aca0f64723a89ede8bbd7fc5d6ffe39a11779dc
unknown <alexvoy.voy@yandex.ru>
2021-04-16 14:44:54 +0300

    back

---
+++ models/movie.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+
+const MovieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  director: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    minlength: 1,
+    maxlength: 10,
+  },
+  year: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 15,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/.test(v);
+      },
+      message: 'Не правильный адрес',
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/.test(v);
+      },
+      message: 'Не правильный адрес',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/.test(v);
+      },
+      message: 'Не правильный адрес',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+    unique: true,
+    minlength: 1,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('movie', MovieSchema);
---
+++ routes/movie.js
@@ -0,0 +1,42 @@
+const movies = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { isURL } = require('validator');
+const {
+  getMovies,
+  newMovie,
+  deleteMovie,
+} = require('../controlles/movie');
+
+movies.get('/', getMovies);
+
+movies.post('/',
+  celebrate({
+    body: Joi.object().keys({
+      country: Joi.string().required().min(2).max(30),
+      director: Joi.string().required().min(2).max(30),
+      duration: Joi.number().required().min(1).max(1000),
+      year: Joi.string().required().min(1).max(15),
+      description: Joi.string().required().min(2).max(50),
+
+      image: Joi.string().required().custom((value, helpers) => {
+        if (!isURL(value)) return helpers.error('Невалидная ссылка');
+        return value;
+      }),
+      trailer: Joi.string().required().custom((value, helpers) => {
+        if (!isURL(value)) return helpers.error('Невалидная ссылка');
+        return value;
+      }),
+      thumbnail: Joi.string().required().custom((value, helpers) => {
+        if (!isURL(value)) return helpers.error('Невалидная ссылка');
+        return value;
+      }),
+      movieId: Joi.number().required().min(1),
+      nameRU: Joi.string().required().min(2).max(30),
+      nameEN: Joi.string().required().min(2).max(30),
+    }),
+  }),
+  newMovie);
+
+movies.delete('/:movieIdDel', deleteMovie);
+
+module.exports = movies;
